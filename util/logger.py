78397a3ee4041894717fb1f955647c33e40578bc
drcpu <drcpu@protonmail.com>
2022-09-14 20:19:42 +0200

    [logger] Move logging functions to util.logger file

--- util/logger.py
+++ util/logger.py
@@ -1,6 +1,9 @@
 import datetime
 import logging
 import os
+import re
+import shutil
+import sys
 import time
 
 def select_logging_level(level):
@@ -40,3 +43,56 @@ def configure_logger(log_tag, log_filename, log_level):
     logger.addHandler(file_handler)
 
     return logger
+
+def configure_logging_listener(config):
+    root = logging.getLogger()
+
+    logging.Formatter.converter = time.gmtime
+
+    # Add header formatting of the log message
+    formatter = logging.Formatter("[%(levelname)-8s] [%(asctime)s] [%(name)-8s] %(message)s", datefmt="%Y/%m/%d %H:%M:%S")
+
+    log_file_name = config["address-jit"]["log"]["log_file"]
+    level_file = select_logging_level(config["address-jit"]["log"]["level_file"])
+    level_stdout = select_logging_level(config["address-jit"]["log"]["level_stdout"])
+
+    # Get log file parts
+    dirname = os.path.dirname(log_file_name)
+    basename = os.path.basename(log_file_name)
+    filename, extension = os.path.splitext(basename)
+    # Move the existing log
+    if os.path.exists(log_file_name):
+        today = datetime.date.today()
+        shutil.move(log_file_name, os.path.join(dirname, f"{filename}.{today.strftime('%Y%m%d')}{extension}"))
+
+    # Add file handler
+    file_handler = logging.handlers.TimedRotatingFileHandler(log_file_name, when="D", utc=True)
+    # Date suffix should not contain dashes
+    file_handler.suffix = "%Y%m%d"
+    file_handler.extMatch = re.compile(r"^\d{8}$")
+    # Put the date timestamp between the filename and the extension
+    file_handler.namer = lambda name: os.path.join(os.path.dirname(name), os.path.basename(name).replace(extension, "") + extension)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(level_file)
+    root.addHandler(file_handler)
+
+    # Add console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(level_stdout)
+    root.addHandler(console_handler)
+
+def create_logging_listener(config, queue):
+    configure_logging_listener(config)
+
+    while True:
+        try:
+            record = queue.get()
+            if record == None:
+                break
+            logger = logging.getLogger(record.name)
+            logger.handle(record)
+        except EOFError:
+            break
+        except KeyboardInterrupt:
+            continue
