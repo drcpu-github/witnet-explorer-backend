7a96feb4521d31b1f1b21efc682b10aab0eddf34
drcpu <drcpu@protonmail.com>
2022-09-30 20:46:27 +0200

    [transactions] Modularize transaction priority calculations for data request and value transfer transactions

--- util/witnet_functions.py
+++
@@ -1,6 +0,0 @@
-def calculate_block_reward(epoch, consensus_constants):
-    halvings = int(epoch // consensus_constants.halving_period)
-    if halvings < 64:
-        return consensus_constants.initial_block_reward >> halvings
-    else:
-        return 0
