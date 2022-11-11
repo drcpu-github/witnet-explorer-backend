eac83d8b6c299b379d8d05e8506460408b070384
drcpu <drcpu@protonmail.com>
2022-11-11 10:26:18 +0100

    [wip] Add mockup functionality to wip class

--- objects/wip.py
+++ objects/wip.py
@@ -7,11 +7,16 @@ from node.witnet_node import WitnetNode
 from util.database_manager import DatabaseManager
 
 class WIP(object):
-    def __init__(self, database_config, node_config):
-        self.db_mngr = DatabaseManager(database_config)
-        self.fetch_wips()
+    def __init__(self, database_config=None, node_config=None, mockup=False):
+        if database_config:
+            self.db_mngr = DatabaseManager(database_config)
+            self.fetch_wips()
 
         self.node_config = node_config
+
+        self.mockup = mockup
+        if self.mockup:
+            self.create_mockup()
 
     def fetch_wips(self):
         sql = """
@@ -32,23 +37,73 @@ class WIP(object):
         """
         self.wips = self.db_mngr.sql_return_all(sql)
 
+    def create_mockup(self):
+        wips = [
+            {
+                "id": 1,
+                "title": "WIP0014-0016",
+                "activation_epoch": 549141,
+            },
+            {
+                "id": 2,
+                "title": "WIP0017-0018-0019",
+                "activation_epoch": 683541,
+            },
+            {
+                "id": 3,
+                "title": "WIP0020-0021",
+                "activation_epoch": 1059861,
+            },
+        ]
+        try:
+            self.set_mockup(wips)
+        except KeyError as e:
+            sys.stderr.write(f"Could not set mockup: {e}")
+            sys.exit(1)
+
+    def set_mockup(self, wips):
+        # Check the passed parameter has the correct type
+        if not isinstance(wips, list):
+            raise TypeError("The wips parameter should be a list of maps")
+        for wip in wips:
+            if not isinstance(wip, dict):
+                raise TypeError("The wips parameter should be a list of maps")
+
+        # Check the required keys were passed
+        required_keys = ["id", "title", "activation_epoch"]
+        for wip in wips:
+            for key in required_keys:
+                if key not in wip:
+                    raise KeyError(f"Key {key} not found in WIP")
+
+        # Transform to list
+        self.wips = [[wip["id"], wip["title"], wip["activation_epoch"]] for wip in wips]
+
     def print_wips(self):
         for wip in self.wips:
-            wip_id, title, description, urls, activation_epoch, tapi_start_epoch, tapi_stop_epoch, tapi_bit = wip
+            if self.mockup:
+                wip_id, title, activation_epoch = wip
+            else:
+                wip_id, title, description, urls, activation_epoch, tapi_start_epoch, tapi_stop_epoch, tapi_bit = wip
             print(f"Entry {wip_id}")
             print(f"\tTitle: {title}")
-            print(f"\tDescription: {description}")
-            for counter, url in enumerate(urls):
-                print(f"\tURL of WIP {counter + 1}: {url}")
+            if not self.mockup:
+                print(f"\tDescription: {description}")
+                for counter, url in enumerate(urls):
+                    print(f"\tURL of WIP {counter + 1}: {url}")
             print(f"\tActivation epoch: {activation_epoch if activation_epoch else 'not activated'}")
-            if tapi_start_epoch:
-                print(f"\tStarted at epoch: {tapi_start_epoch}")
-            if tapi_stop_epoch:
-                print(f"\tStopped at epoch: {tapi_stop_epoch}")
-            if tapi_bit:
-                print(f"\tUsing signaling bit: {tapi_bit}")
+            if not self.mockup:
+                if tapi_start_epoch:
+                    print(f"\tStarted at epoch: {tapi_start_epoch}")
+                if tapi_stop_epoch:
+                    print(f"\tStopped at epoch: {tapi_stop_epoch}")
+                if tapi_bit:
+                    print(f"\tUsing signaling bit: {tapi_bit}")
 
     def add_wip(self):
+        if self.mockup:
+            raise TypeError("Cannot add a WIP on a mockup")
+
         # Read the WIP title
         wip_title = input("Specify the title of the WIP? ")
 
@@ -130,6 +185,10 @@ class WIP(object):
         self.db_mngr.sql_insert_one(sql, (wip_title, wip_description, urls, activation_epoch, tapi_start_epoch, tapi_stop_epoch, tapi_bit))
 
     def process_tapi(self):
+        if self.mockup:
+            sys.stderr.write("Cannot process TAPI signals on a mockup\n")
+            return
+
         witnet_node = WitnetNode(self.node_config)
 
         for wip in self.wips:
@@ -171,20 +230,15 @@ class WIP(object):
                     self.db_mngr.sql_update_table(sql)
 
     def is_wip_active(self, epoch, wip_title):
-        wip_to_check = None
-
-        # Find TAPI of interest based on its id or title
+        # Find TAPI of interest based on its title
         for wip in self.wips:
-            wip_id, title, description, urls, activation_epoch, tapi_start_epoch, tapi_stop_epoch, tapi_bit = wip
+            if self.mockup:
+                wip_id, title, activation_epoch = wip
+            else:
+                wip_id, title, description, urls, activation_epoch, tapi_start_epoch, tapi_stop_epoch, tapi_bit = wip
             if wip_title == title:
-                wip_to_check = wip
-                break
-
-        # Check if it is active based on the activation epoch
-        if wip_to_check:
-            if activation_epoch and epoch >= activation_epoch:
-                return True
-
+                if activation_epoch and epoch >= activation_epoch:
+                    return True
         return False
 
     def is_wip0008_active(self, epoch):
@@ -230,14 +284,41 @@ def main():
 
     config = toml.load(options.config_file)
 
-    wip = WIP(config["database"], config["node-pool"])
-
     # Run some tests
+    wip = WIP(database_config=config["database"], node_config=config["node-pool"])
+
     assert wip.is_wip0008_active(191999) == False
     assert wip.is_wip0008_active(192000) == True
 
     assert wip.is_wip0020_active(1059840) == False
-    assert wip.is_wip0008_active(1059861) == True
+    assert wip.is_wip0020_active(1059861) == True
+
+    mockup_wip = WIP(mockup=True)
+
+    assert mockup_wip.is_wip0008_active(192000) == False
+
+    assert mockup_wip.is_wip0017_active(683541) == True
+    assert mockup_wip.is_wip0018_active(683541) == True
+    assert mockup_wip.is_wip0019_active(683541) == True
+
+    try:
+        mockup_wip.set_mockup([{}])
+        assert False, "This function call should raise a KeyError exception"
+    except Exception as e:
+        assert type(e) == KeyError
+
+    try:
+        mockup_wip.set_mockup([{"id": 1, "title": "WIP0014-0016"}])
+        assert False, "This function call should raise a KeyError exception"
+    except Exception as e:
+        assert type(e) == KeyError
+        assert e.args[0] == "Key activation_epoch not found in WIP"
+
+    try:
+        mockup_wip.add_wip()
+    except Exception as e:
+        assert type(e) == TypeError
+        assert e.args[0] == "Cannot add a WIP on a mockup"
 
 if __name__ == "__main__":
     main()
